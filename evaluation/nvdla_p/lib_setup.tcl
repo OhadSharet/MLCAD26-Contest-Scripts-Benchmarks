511be161d7134ff9e51e11825428436e6868b8e9
TaizunJ <jafri.taizun.s@gmail.com>
2026-03-22 06:30:19 +0000

    Change LIB/LEF paths, moving them outside NLDM folder

--- evaluation/nvdla_p/lib_setup.tcl
+++ evaluation/nvdla_p/lib_setup.tcl
@@ -5,25 +5,27 @@ set std_lef [list \
   /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lef/asap7sc7p5t_28_R_1x_220121a.lef \
   /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lef/asap7sc7p5t_28_SL_1x_220121a.lef \
   /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lef/asap7sc7p5t_28_SRAM_1x_220121a.lef \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lef/sram_asap7_16x256_1rw.lef]
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lef/sram_asap7_16x256_1rw.lef \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lef/sram_asap7_64x256_1rw.lef]
 
 set lib_files [list \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/asap7sc7p5t_AO_LVT_FF_nldm_211120.lib.gz \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/asap7sc7p5t_AO_RVT_FF_nldm_211120.lib.gz \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/asap7sc7p5t_AO_SLVT_FF_nldm_211120.lib.gz \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/asap7sc7p5t_INVBUF_LVT_FF_nldm_220122.lib.gz \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/asap7sc7p5t_INVBUF_RVT_FF_nldm_220122.lib.gz \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/asap7sc7p5t_INVBUF_SLVT_FF_nldm_220122.lib.gz \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/asap7sc7p5t_OA_LVT_FF_nldm_211120.lib.gz \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/asap7sc7p5t_OA_RVT_FF_nldm_211120.lib.gz \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/asap7sc7p5t_OA_SLVT_FF_nldm_211120.lib.gz \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/asap7sc7p5t_SEQ_LVT_FF_nldm_220123.lib \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/asap7sc7p5t_SEQ_RVT_FF_nldm_220123.lib \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/asap7sc7p5t_SEQ_SLVT_FF_nldm_220123.lib \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/asap7sc7p5t_SIMPLE_LVT_FF_nldm_211120.lib.gz \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/asap7sc7p5t_SIMPLE_RVT_FF_nldm_211120.lib.gz \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/asap7sc7p5t_SIMPLE_SLVT_FF_nldm_211120.lib.gz \
-  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/NLDM/sram_asap7_16x256_1rw.lib ]
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/asap7sc7p5t_AO_LVT_FF_nldm_211120.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/asap7sc7p5t_AO_RVT_FF_nldm_211120.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/asap7sc7p5t_AO_SLVT_FF_nldm_211120.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/asap7sc7p5t_INVBUF_LVT_FF_nldm_220122.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/asap7sc7p5t_INVBUF_RVT_FF_nldm_220122.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/asap7sc7p5t_INVBUF_SLVT_FF_nldm_220122.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/asap7sc7p5t_OA_LVT_FF_nldm_211120.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/asap7sc7p5t_OA_RVT_FF_nldm_211120.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/asap7sc7p5t_OA_SLVT_FF_nldm_211120.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/asap7sc7p5t_SEQ_LVT_FF_nldm_220123.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/asap7sc7p5t_SEQ_RVT_FF_nldm_220123.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/asap7sc7p5t_SEQ_SLVT_FF_nldm_220123.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/asap7sc7p5t_SIMPLE_LVT_FF_nldm_211120.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/asap7sc7p5t_SIMPLE_RVT_FF_nldm_211120.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/asap7sc7p5t_SIMPLE_SLVT_FF_nldm_211120.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/sram_asap7_16x256_1rw.lib \
+  /root/MLCAD26-Contest-Scripts-Benchmarks/asap7/lib/sram_asap7_64x256_1rw.lib ]
 
 #reading tech lef
 read_lef $tech_lef
