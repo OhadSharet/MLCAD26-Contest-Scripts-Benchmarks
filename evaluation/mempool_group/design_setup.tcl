a742a7a076c493715147e476f4584351d522e957
jethiraj <jethiraj@en4226528ul.eng.asu.edu>
2026-05-21 12:43:48 -0700

    compressed large def files and untracked git lfs

--- evaluation/mempool_group/design_setup.tcl
+++ evaluation/mempool_group/design_setup.tcl
@@ -1,5 +1,5 @@
 set sdc_file ${top_proj_dir}/benchmarks/${design_name}/mempool_group.sdc
 
 # Replace the original .def and .v files with your modified versions (i.e., the outputs of your developed tool) to evaluate your tool’s performance.
-set def_file ${top_proj_dir}/benchmarks/${design_name}/mempool_group.def
+set def_file ${top_proj_dir}/benchmarks/${design_name}/mempool_group.def.gz
 set verilog_netlist ${top_proj_dir}/benchmarks/${design_name}/mempool_group.v
